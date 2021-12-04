4f897511139ad58fec6071a9f897a01f3b58a0f1
Andrew Jensen <andrew.charles.jensen@gmail.com>
2021-12-04 13:10:18 -0600

    Use a single .gitignore and remove accidentally added derived dfiles.

--- 2021/day3/target/debug/day3.d
+++
@@ -1,1 +0,0 @@
-/Users/acjensen/Documents/GitHub/advent-of-code/2021/day3/target/debug/day3: /Users/acjensen/Documents/GitHub/advent-of-code/2021/day3/src/main.rs
--- 2021/day3/target/debug/deps/day3-602f04b8de9f55fe.d
+++
@@ -1,5 +0,0 @@
-/Users/acjensen/Documents/GitHub/advent-of-code/2021/day3/target/debug/deps/day3-602f04b8de9f55fe: src/main.rs
-
-/Users/acjensen/Documents/GitHub/advent-of-code/2021/day3/target/debug/deps/day3-602f04b8de9f55fe.d: src/main.rs
-
-src/main.rs:
