e3b1d3b39a569d114f5fc3009b1ef6c8828db586
Andrew Jensen <acjensen@amazon.com>
2023-12-09 09:34:15 -0600

    Refactor 2023 day7

--- 2023/day7/day7.py
+++ 2023/day7/day7.py
@@ -13,148 +13,48 @@ def read_input_lines(filename):
 
 for _input in ["input_example", "input"]:
 
-    def pt1():
-        lines = read_input_lines(_input)
-        stuff = [l.split() for l in lines]
-
-        bids = dict()
-        for hand, bid in stuff:
-            bids[hand] = int(bid)
-
-        card_weight = "A, K, Q, J, T, 9, 8, 7, 6, 5, 4, 3, 2".replace(
-            ',', '').split()
-        card_weight.reverse()
-
-        card_weights = {k: v for v, k in enumerate(card_weight)}
-
-        def get_hand_type(hand):
-            d = defaultdict(int)
-            for card in hand:
-                d[card] += 1
-            if [5] == sorted(list(d.values())):
-                return 1
-            elif [1, 4] == sorted(list(d.values())):
-                return 2
-            elif [2, 3] == sorted(list(d.values())):
-                return 3
-            elif [1, 1, 3] == sorted(list(d.values())):
-                return 4
-            elif [1, 2, 2] == sorted(list(d.values())):
-                return 5
-            elif [1, 1, 1, 2] == sorted(list(d.values())):
-                return 6
-            elif [1, 1, 1, 1, 1] == sorted(list(d.values())):
-                return 7
-            return None
-
-        def sortt(i, groups):
-            more_sorting_to_do = False
-            result = []
-            for group in groups:
-                if len(group) == 1:
-                    result.append(group)
-                    continue
-                _sorted = sorted(
-                    group, key=lambda hand: card_weights[hand[i]])
-                prev = _sorted[0][i]
-                g = [_sorted[0]]
-                for hand in _sorted[1:]:
-                    if prev != hand[i]:
-                        result.append(g)
-                        g = [hand]
-                    else:
-                        more_sorting_to_do = True
-                        g.append(hand)
-                    prev = hand[i]
-                if g:
-                    result.append(g)
-            return result, more_sorting_to_do
-
-        d = defaultdict(list)
-        for hand, bid in stuff:
-            d[get_hand_type(hand)].append(hand)
-        # print(d)
-        d_2 = dict()
-        for k, hands in d.items():
-            hands_sorted = hands
-            for i in range(len(hands[0])):
-                # print('hands_sorted', hands_sorted)
-                hands_sorted = sorted(
-                    hands, key=lambda hand: card_weights[hand[i]])
-            d_2[k] = hands_sorted
-        # print(d_2)
-
-        r = []
-        ks = sorted(d_2.keys())
-        ks.reverse()
-        for k in ks:
-            r.append(d_2[k])
-        # print(r)
-
-        more_sorting_to_do = True
-        i = 0
-        rr = r
-        while more_sorting_to_do or i == 5:
-            rr, more_sorting_to_do = sortt(i, rr)
-            i += 1
-        # print('rr', rr)
-
-        ans = 0
-        for idx, r in enumerate(rr):
-            # print('math', r[0], (idx+1), '*', bids[r[0]])
-            ans += (idx+1)*bids[r[0]]
-        print("pt1", _input, ans)
-
-    pt1()
-
-    def pt2():
+    def pt1_pt2(is_part_1=False):
         stuff = [l.split() for l in read_input_lines(_input)]
 
         bids = dict()
         for hand, bid in stuff:
             bids[hand] = int(bid)
 
-        card_weight = "A, K, Q, T, 9, 8, 7, 6, 5, 4, 3, 2, J".replace(
-            ',', '').split()
+        card_weight = "A, K, Q, J, T, 9, 8, 7, 6, 5, 4, 3, 2".replace(
+            ',', '').split() if is_part_1 else "A, K, Q, T, 9, 8, 7, 6, 5, 4, 3, 2, J".replace(
+                ',', '').split()
         card_weight.reverse()
         card_weights = {k: v for v, k in enumerate(card_weight)}
 
         def get_hand_type(hand: str):
-            d = defaultdict(int)
-            for card in hand:
-                if card != 'J':
-                    d[card] += 1
-            if 'J' in hand:
-                vs = sorted(d.values())
-                vs.reverse()
-                def get_top_letter():
-                    for k, v in d.items():
-                        if v == vs[0]:
-                            return k
-                    return None
-                top_letter = get_top_letter()
-                if top_letter:
-                    hand = hand.replace('J', top_letter)
+            if not is_part_1:
+                # Replace all 'J' with value of highest card count
+                d = defaultdict(int)
+                for card in hand:
+                    if card != 'J':
+                        d[card] += 1
+                if 'J' in hand:
+                    vs = sorted(d.values())
+                    vs.reverse()
+                    def get_top_letter():
+                        for k, v in d.items():
+                            if v == vs[0]:
+                                return k
+                        return None
+                    top_letter = get_top_letter()
+                    if top_letter:
+                        hand = hand.replace('J', top_letter)
 
-            d = defaultdict(int)
+            hand_types = [[5], [1, 4], [2, 3], [1, 1, 3], [1, 2, 2], [1, 1, 1, 2], [1, 1, 1, 1, 1]]
+            count_per_card = defaultdict(int)
             for card in hand:
-                d[card] += 1
-            if [5] == sorted(list(d.values())):
-                return 1
-            elif [1, 4] == sorted(list(d.values())):
-                return 2
-            elif [2, 3] == sorted(list(d.values())):
-                return 3
-            elif [1, 1, 3] == sorted(list(d.values())):
-                return 4
-            elif [1, 2, 2] == sorted(list(d.values())):
-                return 5
-            elif [1, 1, 1, 2] == sorted(list(d.values())):
-                return 6
-            elif [1, 1, 1, 1, 1] == sorted(list(d.values())):
-                return 7
+                count_per_card[card] += 1
+            for idx, hand_type in enumerate(hand_types):
+                rank = idx + 1
+                if hand_type == sorted(list(count_per_card.values())):
+                    return rank
             return None
-    
+
 
         def sort_by_card_weight(i, groups):
             result = []
@@ -177,39 +77,34 @@ for _input in ["input_example", "input"]:
                     result.append(g)
             return result
 
-        d = defaultdict(list)
-        for hand, _ in stuff:
-            d[get_hand_type(hand)].append(hand)
-        d_2 = dict()
-        for k, hands in d.items():
-            hands_sorted = hands
-            for i in range(len(hands[0])):
-                hands_sorted = sorted(
-                    hands, key=lambda hand: card_weights[hand[i]])
-            d_2[k] = hands_sorted
-
-        r = []
-        ks = sorted(d_2.keys())
-        ks.reverse()
-        for k in ks:
-            r.append(d_2[k])
+        def sort_by_hand_type():
+            hands_by_hand_type = defaultdict(list)
+            for hand, _ in stuff:
+                hands_by_hand_type[get_hand_type(hand)].append(hand)
+            d = dict()
+            for hand_type, hands in hands_by_hand_type.items():
+                hands_sorted = hands
+                for i in range(len(hands[0])):
+                    hands_sorted = sorted(hands, key=lambda hand: card_weights[hand[i]])
+                d[hand_type] = hands_sorted
+            hands = []
+            for k in reversed(sorted(d.keys())):
+                hands.append(d[k])
+            return hands
+        
+        hands = sort_by_hand_type()
 
         i = 0
-        rr = r
-        while i != 5:
-            rr = sort_by_card_weight(i, rr)
+        total_cards_per_hand = len(stuff[0][0])
+        for i in range(0, total_cards_per_hand):
+            hands = sort_by_card_weight(i, hands)
             i += 1
 
+        # Calculate the answer based on rank and bid
         ans = 0
-        for idx, r in enumerate(rr):
-            ans += (idx+1)*bids[r[0]]
-        print("pt2", _input, ans)
+        for idx, _hands in enumerate(hands):
+            ans += (idx+1)*bids[_hands[0]]
+        print("pt1" if is_part_1 else "pt2", _input, ans)
 
-    pt2()
-
-"""
-pt1 input_example 6440
-pt2 input_example 5905
-pt1 input 253910319
-pt2 input 254083736
-"""
+    pt1_pt2(is_part_1=True)
+    pt1_pt2(is_part_1=False)
